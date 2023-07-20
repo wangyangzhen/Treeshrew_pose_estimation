4f99a12c141e8f9c9402809a5b9172603a5734d5
ZhangChenLab <ZhangChenLab@163.com>
2023-07-21 01:35:42 +0800

    update

--- RF_model.py
+++ RF_model.py
@@ -5,24 +5,42 @@ Created on Fri Jul 14 17:15:48 2023
 @author: yangzhen
 """
 
-
+import pandas as pd
 import h5py
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 import joblib
 
-# Read the 'demo_movieDLC_resnet50_treeshew_DLCJan1shuffle1_150000.h5' file
-with h5py.File('demo_movieDLC_resnet50_treeshew_DLCJan1shuffle1_150000.h5', 'r') as file:
-    x = file['data'][:]  # Assuming the dataset is named 'data'
 
-# Read the 'label.h5' file
-with h5py.File('label.h5', 'r') as file:
-    y = file['labels'][:]  # Assuming the dataset is named 'labels'
 
-# Create a RandomForestClassifier model
-model = RandomForestClassifier()
+file_X = './demo_movieDLC_resnet50_treeshew_DLCJan1shuffle1_100000.h5'
 
-# Train the model using the data
-model.fit(x, y)
+file_Y = '.movie/demo_movie.h5'
+
+coor = pd.read_hdf(file_X, "df_with_missing").values
+
+with h5py.File(file_Y, 'r') as file:
+    video_label = file['video_label'][:]
+
+# Prepare the data for training
+X_train = []
+y_train = []
+for i in range(len(video_label)):
+    if video_label[i] != -1:
+        # Include the data in training set if the label is not -1
+        X_train.append(coor[i])
+        y_train.append(video_label[i])
+
+# Create and train the random forest classifier
+rf_classifier = RandomForestClassifier()
+rf_classifier.fit(X_train, y_train)
+
+# Calculate the accuracy on the training set
+y_train_pred = rf_classifier.predict(X_train)
+accuracy = accuracy_score(y_train, y_train_pred)
+print("Training Accuracy:", accuracy)
 
 # Save the trained model
-joblib.dump(model, 'demo_movie_RF.pkl')
+model_file = 'path_to_save_model.pkl'
+joblib.dump(rf_classifier, model_file)
+print("Model saved successfully.")
